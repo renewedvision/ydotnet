19bd1e4e9ac40270ef88cff7487cf1b12949db0a
Zachariah Brown <zachariah@renewedvision.com>
2024-05-23 15:14:39 -0400

    Resolve versioning issues

--- publish.sh
+++ publish.sh
@@ -5,5 +5,5 @@ read GH_API_KEY
 
 dotnet pack YDotNet.sln --configuration Release
 
-dotnet nuget push "YDotNet\bin\Release\YDotNet.1.0.0.nupkg" --source "github" --api-key "$GH_API_KEY"
-dotnet nuget push "native\YDotNet.Native.Win32\bin\Release\YDotNet.Native.Win32.0.4.0.nupkg" --source "github" --api-key "$GH_API_KEY"
+dotnet nuget push "YDotNet\bin\Release\RenewedVision.YDotNet.0.4.0.nupkg" --source "github" --api-key "$GH_API_KEY"
+dotnet nuget push "native\YDotNet.Native.Win32\bin\Release\RenewedVision.YDotNet.Native.Win32.0.4.0.nupkg" --source "github" --api-key "$GH_API_KEY"
