23fa8f69afbe5be4302b83339a34229518291cdd
Zachariah Brown <zachariah@renewedvision.com>
2024-05-23 15:21:37 -0400

    Fix runtime crash (caused by porting)

--- ext/RandomHelper.cs
+++ ext/RandomHelper.cs
@@ -2,7 +2,7 @@ namespace System
 {
     public static class RandomHelper
     {
-        private static readonly ThreadLocal<Random> _sharedRandom = new ThreadLocal<Random>();
+        private static readonly ThreadLocal<Random> _sharedRandom = new ThreadLocal<Random>(() => new());
 
         public static Random Shared => _sharedRandom.Value;
     }
